272976ba412c437c39dfe733958091c0cb35875e
VelizarT <velizar_tonchev@abv.bg>
2018-09-18 14:15:49 +0100

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log('App is running');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = (
+    <div>
+        <p>Template</p>
+    </div>
+);
+
+ReactDOM.render(template, document.getElementById('app'));
